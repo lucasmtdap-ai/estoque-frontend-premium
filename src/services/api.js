1f8b17a3698f64c9825dbd44dbfe748e0cd62563
lucasmtdap-ai <lucasmtdap@gmail.com>
2026-04-05 16:32:55 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,7 @@
 import axios from "axios";
-import { getToken } from "./auth";
 
 const api = axios.create({
   baseURL: "https://stock-backend-hp9t.onrender.com"
-});
-
-api.interceptors.request.use((config) => {
-  const token = getToken();
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
 });
 
 export default api;
