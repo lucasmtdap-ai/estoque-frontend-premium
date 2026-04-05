0aa78913440a18c44af3e815405f27ebc0108406
lucasmtdap-ai <lucasmtdap@gmail.com>
2026-04-05 17:30:38 -0300

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import { getToken } from "../services/auth.js";
 
 export default function ProtectedRoute({ children }) {
-  const token = localStorage.getItem("token");
+  const token = getToken();
 
   if (!token) {
     return <Navigate to="/login" replace />;
