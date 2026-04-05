0e148e2442cbf8f68dbb98fa34502cd9847b56eb
lucasmtdap-ai <lucasmtdap@gmail.com>
2026-04-05 13:11:45 -0300

    Create auth.js

---
+++ src/services/auth.js
@@ -0,0 +1,17 @@
+export function salvarAuth(data) {
+  localStorage.setItem("token", data.token);
+  localStorage.setItem("user", JSON.stringify(data.user));
+}
+
+export function getToken() {
+  return localStorage.getItem("token");
+}
+
+export function getUser() {
+  return JSON.parse(localStorage.getItem("user"));
+}
+
+export function logout() {
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+}
