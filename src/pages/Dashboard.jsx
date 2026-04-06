4b44e7a404da6c54de904dc4e0f8f56bd41718ca
lucasmtdap-ai <lucasmtdap@gmail.com>
2026-04-05 21:20:59 -0300

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import api from "../services/api.js";
 
 export default function Dashboard() {
-  const user = let user = {};
+let user = {};
 
 try {
   user = JSON.parse(localStorage.getItem("user") || "{}");
